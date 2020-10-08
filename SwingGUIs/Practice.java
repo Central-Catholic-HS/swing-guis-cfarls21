ad0d2b973c93dd3aa5e1aceafe299920c24c1ee5
Christian Farls (sex symbol) <72447959+cfarls21@users.noreply.github.com>
2020-10-07 23:11:59 -0400

    Add files via upload

---
+++ SwingGUIs/Practice.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class Practice
+{
+    public static void main(String[] args)
+    {
+        String firstName;
+        String middleName;
+        String lastName;
+        String truthFirst = "Anthony";
+        String truthMiddle = "Michael";
+        String truthLast = "Gatti";
+        
+        firstName = JOptionPane.showInputDialog("What is your lover's first name?");
+        middleName = JOptionPane.showInputDialog("What is your lover's second name?");
+        lastName = JOptionPane.showInputDialog("What is your lover's last name?");
+        
+        //JOptionPane.showMessageDialog(null, "You may be horny for " + firstName + " " + 
+        //                              middleName + " " + lastName + ", but you really love " +
+        //                              truthFirst + " " + truthMiddle + " " + truthLast + "...");
+        JOptionPane.showMessageDialog(null, "fuck you");    
+                                      
+        System.exit(0);
+        
+    }
+}
